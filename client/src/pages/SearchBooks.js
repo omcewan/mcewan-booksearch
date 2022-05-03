eddd77d79678c9880aab16b6df0da110890d2b0e
omcewan <orlandomcewan231@gmail.com>
2022-05-03 17:34:14 -0400

    update searchbooks.js to save books to user when logged in

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -61,6 +61,8 @@ const SearchBooks = () => {
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
+    console.log(typeof bookToSave)
+
 
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
@@ -70,12 +72,12 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook({variables: bookToSave});
-      console.log(response)
+      const {data} = await saveBook({variables: {input: bookToSave}});
+      console.log(data)
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+      // }
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
